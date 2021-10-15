9961061145073c040145ac082b71706e15fdb8e0
Nolan Caine <nolancaine@gmail.com>
2021-10-14 21:23:34 -0600

    Display rating & release

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
-const Card = ({ poster }) => {
+const Card = ({ poster, rating, release }) => {
   return (
     <article>
       <img src={poster} />
+      <p>{rating}</p>
+      <p>{release}</p>
     </article>
   )
 }
