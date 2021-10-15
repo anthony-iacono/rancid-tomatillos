bbb13a947af76753f7418fc84d1cef1fdde9c64c
Anthony Iacono <72999840+anthony-iacono@users.noreply.github.com>
2021-10-14 19:36:50 -0700

    Flesh out Gallery

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,10 +1,15 @@
 import React from 'react'
 // import Card from './Card'
 
-const Gallery = () => {
+const Gallery = ({ movies }) => {
+
+  const movieCards = movies.map(movie => <Card
+    poster={movie.poster_path}
+  />)
+
   return (
     <section>
-      {/* <Card /> */}
+      {movieCards}
     </section>
   )
 }
