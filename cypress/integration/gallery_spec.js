d38eac601275b3fa304f35d1fc467b32d9a06de2
Anthony Iacono <72999840+anthony-iacono@users.noreply.github.com>
2021-10-18 19:33:23 -0700

    Write partial test for displaying movie details

--- cypress/integration/gallery_spec.js
+++ cypress/integration/gallery_spec.js
@@ -12,15 +12,30 @@ describe('Gallery', () => {
   })
 
   it('should display movie cards within the gallery', () => {
-
+    cy.get('.movie-card')
   })
 
-  it('should display clickable movie cards', () => {
-
-  })
-
-  it('should redirect to detail view when movie card is clicked', () => {
-
+  it('should display movie details when a movie card is clicked', () => {
+    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies/694919', {
+      body : {
+        "movie": {
+          "id": 694919,
+          "title": "Money Plane",
+          "poster_path": "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg",
+          "backdrop_path": "https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg",
+          "release_date": "2020-09-29",
+          "overview": "A professional thief with $40 million in debt and his family's life on the line must commit one final heist - rob a futuristic airborne casino filled with the world's most dangerous criminals.",
+          "genres": [
+              "Action"
+          ],
+          "budget": 0,
+          "revenue": 0,
+          "runtime": 82,
+          "tagline": "",
+          "average_rating": 6.142857142857143
+        }
+      }
+    })
   })
 
   it('should show additional info when a card is hovered over', () => {
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Card = ({ id, poster, rating, release, displayDetails }) => {
   return (
-    <article onClick={ () => displayDetails(id) }>
+    <article
+      className='movie-card'
+      onClick={ () => displayDetails(id) }
+    >
       <img src={poster || 'https://upload.wikimedia.org/wikipedia/commons/6/65/No-Image-Placeholder.svg'} alt=''/>
       <p>{rating}</p>
       <p>{release}</p>
