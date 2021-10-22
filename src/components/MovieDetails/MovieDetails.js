59b6093d401d1a00c78c1cb9279910b9d1e2c2e4
Anthony Iacono <72999840+anthony-iacono@users.noreply.github.com>
2021-10-22 12:45:50 -0700

    Handle error messages displayed in MovieDetails

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -9,7 +9,9 @@ class MovieDetails extends Component {
     super(props)
     this.state = {
       movie: null,
-      videos: null
+      videos: null,
+      error: '',
+      status: 'loading'
     }
   }
 
@@ -19,50 +21,59 @@ class MovieDetails extends Component {
       api.getSingleMovieVideos(this.props.id)
     ])
       .then(data => {
-        this.setState({ movie: data[0], videos: data[1] })
+        this.setState({ movie: data[0], videos: data[1], status: 'success', error: '' })
+      })
+      .catch(error => {
+        this.setState({ error: error.message, status: 'error'})
       })
   }
 
   formatReleaseDate() {
-    const { movie, videos } = this.state;
+    const { movie } = this.state;
     const [ year, month, day ] = movie.release_date.split("-")
-    console.log(year);
-    return year;
+    return `${month} ${day} ${year}`
   }
 
   formatRating() {
-    const { movie, videos } = this.state;
+    const { movie } = this.state;
     const { averageRating } = movie;
     return Math.round(movie.average_rating * 10) + "%";
   }
 
   formatGenres() {
-    const { movie, videos } = this.state;
+    const { movie } = this.state;
     return movie.genres.join("/");
   }
 
   render() {
+    const status = this.state.status
     const { movie, videos } = this.state;
-    return !this.state.movie ? <h1>Loading</h1> : (
-      <section className="details-container">
-        <YouTubeVideo videoKey={videos[videos.length - 1].key}/>
-        <section
-          className="details"
-          style={{backgroundImage: `url(${this.state.movie.backdrop_path})`}}
-        >
-          <article className="details-wrapper">
-            <div className="heading">
-              <h2>{movie.title} ({this.formatReleaseDate()})</h2>
-            {movie.tagline && <h3>{`${movie.tagline}`}</h3>}
-            </div>
-            <p>{movie.overview}</p>
-            <h4>{this.formatGenres()} - {movie.runtime} minutes - {this.formatRating()} rating</h4>
-            <Link to='/'>
-              <button>Home</button>
-            </Link>
-          </article>
-        </section>
-      </section>
+    return (
+      <>
+        {status === 'loading' && <h1 className='message'>Loading</h1>}
+        {status === 'error' && <h1 className='message'>{this.state.error}</h1>}
+        {status === 'success' && this.state.movie &&
+          <section className="details-container">
+            <YouTubeVideo videoKey={videos[videos.length - 1].key}/>
+            <section
+              className="details"
+              style={{backgroundImage: `url(${this.state.movie.backdrop_path})`}}
+            >
+              <article className="details-wrapper">
+                <div className="heading">
+                  <h2>{movie.title} ({this.formatReleaseDate()})</h2>
+                  {movie.tagline && <h3>{`${movie.tagline}`}</h3>}
+                </div>
+                <p>{movie.overview}</p>
+                <h4>{this.formatGenres()} - {movie.runtime} minutes - {this.formatRating()} rating</h4>
+                <Link to='/'>
+                  <button>Home</button>
+                </Link>
+              </article>
+            </section>
+          </section>
+        }
+      </>
     )
   }
 }
