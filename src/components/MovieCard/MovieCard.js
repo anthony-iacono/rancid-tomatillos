b9e58fcd8dd0c040124cb45d6e0d33d714d0165c
Anthony Iacono <72999840+anthony-iacono@users.noreply.github.com>
2021-10-22 19:11:34 -0700

    Add title to MovieCard

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './MovieCard.css'
 
-const MovieCard = ({ id, poster_path, average_rating, release_date }) => {
+const MovieCard = ({ id, title, poster_path, average_rating, release_date }) => {
   return (
     <Link to={`/movies/${id}`}>
       <article className='movie-card' style={{
@@ -10,6 +10,7 @@ const MovieCard = ({ id, poster_path, average_rating, release_date }) => {
         backgroundSize: 'contain'
       }}>
         <div className='text-overlay'>
+          <h2>{title}</h2>
           <p>Average Rating:
             <span className="yellow">{average_rating.toFixed(1)} / 10</span>
           </p>
