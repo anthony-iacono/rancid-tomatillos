4b12eebf423738dd5e7ff88734fddacb4a7e39b8
Nolan Caine <nolancaine@gmail.com>
2021-10-23 00:50:43 -0600

    Add login with localStorage

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,19 +5,20 @@ import './Header.css'
 
 import searchIcon from '../../assets/img_197388.png'
 
-
-const Header = () => {
+const Header = ({ loggedIn }) => {
   return (
     <header>
       <Link to='/' >
         <h1>CinemaCentral</h1>
       </Link>
-      <nav>
-        <span className="search">
-          <img className="search-icon" onClick={() => document.querySelector(".search-bar").classList.toggle("active")} src={searchIcon} alt='search icon' />
-          <input className="search-bar" type="text" placeholder="search by movie or genre..."/>
-        </span>
-      </nav>
+      {loggedIn &&
+        <nav>
+          <span className="search">
+            <img className="search-icon" onClick={() => document.querySelector(".search-bar").classList.toggle("active")} src={searchIcon} alt='search icon' />
+            <input className="search-bar" type="text" placeholder="search by movie or genre..."/>
+          </span>
+        </nav>
+      }
     </header>
   )
 }
---
+++ src/components/Login/Login.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import './Login.css'
+import { Link } from 'react-router-dom'
+
+const Login = ({ email, password, error, handleChange, handleSubmit }) => {
+  return (
+    <form>
+      {error && <p className="message">{error}</p>}
+      <input
+        type='email'
+        placeholder='email'
+        name='email'
+        value={email}
+        onChange={handleChange}
+        required
+        className='text-field'
+      />
+      <input
+        type='password'
+        placeholder='password'
+        name='password'
+        value={password}
+        onChange={handleChange}
+        required
+        className='text-field'
+      />
+      <Link to='/' onClick={handleSubmit}>
+        <button className='login-btn'>Log in</button>
+      </Link>
+    </form>
+  )
+}
+
+export default Login
