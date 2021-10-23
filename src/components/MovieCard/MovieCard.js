b52259f8ab36fcce55b986a557149345550e84db
Anthony Iacono <72999840+anthony-iacono@users.noreply.github.com>
2021-10-22 18:29:23 -0700

    Formate MovieCard attributes for readability

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -5,10 +5,17 @@ import './MovieCard.css'
 const MovieCard = ({ id, poster_path, average_rating, release_date }) => {
   return (
     <Link to={`/movies/${id}`}>
-      <article className='movie-card' style={{backgroundImage: `url(${poster_path})`, backgroundSize: 'contain'}}>
+      <article className='movie-card' style={{
+        backgroundImage: `url(${poster_path})`,
+        backgroundSize: 'contain'
+      }}>
         <div className='text-overlay'>
-          <p>Average Rating: <span className="yellow">{average_rating.toFixed(1)} / 10</span></p>
-          <p>Released: <span className="yellow">{release_date}</span></p>
+          <p>Average Rating:
+            <span className="yellow">{average_rating.toFixed(1)} / 10</span>
+          </p>
+          <p>Released:
+            <span className="yellow">{release_date}</span>
+          </p>
         </div>
       </article>
     </Link>
