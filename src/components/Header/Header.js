2d4b212f31a4fd5df4ef80fcb3b5c2fc803999fa
Nolan Caine <nolancaine@gmail.com>
2021-10-23 01:13:06 -0600

    Add mascot with animation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,12 +4,16 @@ import { Link } from 'react-router-dom'
 import './Header.css'
 
 import searchIcon from '../../assets/img_197388.png'
+import clippy from '../../assets/reel_swell_guy.png'
 
 const Header = ({ loggedIn }) => {
   return (
     <header>
       <Link to='/' >
-        <h1>CinemaCentral</h1>
+        <article className="title-with-logo">
+          <h1>CinemaCentral</h1>
+          <img className="clippy" src={clippy} alt='cartoon film reel mascot' />
+        </article>
       </Link>
       {loggedIn &&
         <nav>
